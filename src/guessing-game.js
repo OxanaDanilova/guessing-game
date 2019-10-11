01ab9819fab3224188d264310c1f8be472fd36f2
OxanaDanilova <oxanadanilova11@gmail.com>
2019-10-11 16:59:39 +0200

    feat: resolve task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.number = undefined;
+    }
 
     setRange(min, max) {
+      this.min = min;
+      this.max = max;
 
     }
 
     guess() {
-
+      this.number = Math.round((this.max-this.min)/2 + this.min);
+      return this.number;
     }
 
     lower() {
+      this.max = this.number;
+      this.number = Math.round((this.number-this.min)/2 + this.min);
+      return this.number;
 
     }
 
     greater() {
+      this.min = this.number;
+      this.number = Math.round(this.max - (this.max-this.number)/2);
+      return this.number;
 
     }
 }
